41e8741c64c999750c421d9dd1363c8684cde0b9
Dennis Palmer <dodava@gmail.com>
2023-08-17 22:49:58 +0200

    adds ReadmeTest

---
+++ test/readme_test.exs
@@ -0,0 +1,10 @@
+defmodule ReadmeTest do
+  use ExUnit.Case, async: true
+
+  test "version in readme matches mix.exs" do
+    readme_markdown = File.read!(Path.join(__DIR__, "../README.md"))
+    mix_config = Mix.Project.config()
+    version = mix_config[:version]
+    assert readme_markdown =~ ~s({:decorum, "~> #{version}"})
+  end
+end
