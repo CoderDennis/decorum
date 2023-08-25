aef08ced8b4f7b2f38bec00838bd383857d7a3dc
Dennis Palmer <dodava@gmail.com>
2023-08-25 06:12:54 +0200

    moves next and get_history implementations into submodules

--- lib/prng.ex
+++ lib/prng.ex
@@ -15,8 +15,6 @@ defmodule PRNG do
   @type prng :: PRNG.Random.t() | PRNG.Hardcoded.t()
   @type history :: list(non_neg_integer())
 
-  @int32 Integer.pow(2, 32)
-
   defmodule Random do
     @moduledoc false
     @type t :: %__MODULE__{state: :rand.state(), history: PRNG.history()}
@@ -24,11 +22,22 @@ defmodule PRNG do
     @enforce_keys [:state, :history]
     defstruct [:state, :history]
 
+    @int32 Integer.pow(2, 32)
+
     @spec new(seed :: non_neg_integer()) :: t
     def new(seed) do
       state = :rand.seed(:exsss, {seed, seed, seed})
       %__MODULE__{state: state, history: []}
     end
+
+    @spec next(prng :: t()) :: {non_neg_integer(), t}
+    def next(%__MODULE__{state: state, history: history} = prng) do
+      {value, new_state} = :rand.uniform_s(@int32, state)
+      {value, %__MODULE__{prng | state: new_state, history: [value | history]}}
+    end
+
+    @spec get_history(prng :: t()) :: PRNG.history()
+    def get_history(%__MODULE__{history: history}), do: Enum.reverse(history)
   end
 
   defmodule Hardcoded do
@@ -42,29 +51,31 @@ defmodule PRNG do
     def new(history) when is_list(history) do
       %__MODULE__{wholeHistory: history, unusedHistory: history}
     end
+
+    @spec next(prng :: t()) :: {non_neg_integer() | :error, t()}
+    def next(%__MODULE__{unusedHistory: []} = prng) do
+      {:error, prng}
+    end
+
+    def next(%__MODULE__{unusedHistory: [value | rest]} = prng) do
+      {value, %__MODULE__{prng | unusedHistory: rest}}
+    end
+
+    @spec get_history(prng :: t()) :: PRNG.history()
+    def get_history(%__MODULE__{wholeHistory: history}), do: history
   end
 
   @spec random(seed :: non_neg_integer()) :: t
   defdelegate random(seed), to: PRNG.Random, as: :new
 
-  @spec hardcoded(history :: history) :: t
+  @spec hardcoded(history :: history()) :: t
   defdelegate hardcoded(history), to: PRNG.Hardcoded, as: :new
 
-  @spec next(t()) :: {non_neg_integer() | :error, t()}
-  def next(%PRNG.Random{state: state, history: history} = prng) do
-    {value, new_state} = :rand.uniform_s(@int32, state)
-    {value, %PRNG.Random{prng | state: new_state, history: [value | history]}}
-  end
+  @spec next(prng :: t()) :: {non_neg_integer() | :error, t()}
+  def next(%PRNG.Random{} = prng), do: PRNG.Random.next(prng)
+  def next(%PRNG.Hardcoded{} = prng), do: PRNG.Hardcoded.next(prng)
 
-  def next(%PRNG.Hardcoded{unusedHistory: []} = prng) do
-    {:error, prng}
-  end
-
-  def next(%PRNG.Hardcoded{unusedHistory: [value | rest]} = prng) do
-    {value, %PRNG.Hardcoded{prng | unusedHistory: rest}}
-  end
-
-  @spec get_history(t()) :: history()
-  def get_history(%PRNG.Random{history: history}), do: Enum.reverse(history)
-  def get_history(%PRNG.Hardcoded{wholeHistory: history}), do: history
+  @spec get_history(prng :: t()) :: history()
+  def get_history(%PRNG.Random{} = prng), do: PRNG.Random.get_history(prng)
+  def get_history(%PRNG.Hardcoded{} = prng), do: PRNG.Hardcoded.get_history(prng)
 end
